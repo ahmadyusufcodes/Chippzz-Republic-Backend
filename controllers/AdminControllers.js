9d8006702eb2e86b965a1875a13e94436eb382c4
Ahmad Yusuf <35657456+Ahmad-opera@users.noreply.github.com>
2022-06-01 23:37:15 +0100

    Changes to date

--- controllers/AdminControllers.js
+++ controllers/AdminControllers.js
@@ -378,16 +378,6 @@ module.exports.get_summary_date_to_date = async(req, res) => {
     const start = new Date(fromDate)
     const end = new Date(toDate)
 
-    // start.setHours(0)
-    // start.setMinutes(0)
-    // start.setSeconds(0)
-    // end.setHours(0)
-    // end.setMinutes(0)
-    // end.setSeconds(0)
-
-    start.setDate(start.getDate() - 1)
-    end.setDate(end.getDate() + 1)
-    
     
     try {
         const getOrders = await Order.find({createdAt: {$gt: start.toISOString(), $lt: end.toISOString()}, $or: [{orderType: "Instant-Order"}, {orderType: "Shipment"}]})
